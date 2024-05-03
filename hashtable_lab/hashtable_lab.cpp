586dc418e8c64d80049174f123e48b5b4da2817b
rodio <rodionmarkelov01229@gmail.com>
2024-05-03 14:09:40 +0400

    final class

--- hashtable_lab/hashtable_lab.cpp
+++ hashtable_lab/hashtable_lab.cpp
@@ -5,27 +5,102 @@
 
 using namespace std;
 
-//int hashfun(int key) {
-//	if (key < 0) throw runtime_error("Key cann`t be neggative.");
-//	int tmp = key * A;
-//	int result = ((result >> L) | (result << WORD_LENGHT - L));
-//	return result % 2;
-//}
-
 int main()
 {
-	HashTable<int, int> hashtable_1(2);
+	/////////////////////////////////////////////////////
+	// TEST CLASS
+	/////////////////////////////////////////////////////
+	HashTable<int, int> hashtable_1(4);
 	hashtable_1.print();
-	cout << "capacity = " << hashtable_1.get_capacity() << " " << "size = " << hashtable_1.get_size() << endl;
 	hashtable_1.insert(0, 2);
-	hashtable_1.insert(1, 3);
-	hashtable_1.insert_or_assign(0,2);
-
-	cout << hashtable_1.contains(2) << endl;
-	cout << hashtable_1.erase(0) << endl;
-	
+	hashtable_1.insert(43684578, 17);
+	hashtable_1.insert(436848, 6);
+	hashtable_1.insert(26507, 5);
+	hashtable_1.insert(26, 7);
 	hashtable_1.print();
-	cout << "capacity = " << hashtable_1.get_capacity() << " " << "size = " << hashtable_1.get_size() << endl;
 
-	/*cout << hashfun(1);*/
+	cout << "Search " << hashtable_1.search(0) << ' ' << hashtable_1.search(25) << endl;
+	cout << "Contains:" << hashtable_1.contains(2) << ' ' << hashtable_1.contains(10004) << endl;
+
+
+	hashtable_1.insert_or_assign(6, 5);
+	hashtable_1.insert_or_assign(26, 101);
+	hashtable_1.print();
+
+
+	hashtable_1.erase(26);
+	hashtable_1.erase(25);
+	hashtable_1.print();
+
+
+	cout << "Copy " << endl;
+	HashTable<int, int> hashtable_2(hashtable_1);
+	hashtable_2.print();
+
+
+	cout << "Operator = " << endl;
+	HashTable<int, int> hashtable_3 = hashtable_1;
+	hashtable_3.print();
+
+
+	HashTable<int, int> hashtable_4(64, 0, 1000, -100, 0);
+	hashtable_4.print();
+
+
+	HashTable<double, int> hashtable_double_int(8, 1, 100, 4.0, 20.0);
+	hashtable_double_int.print();
+
+
+	HashTable<double, double> hashtable_double_double(8, 1.0, 100.0, 4.0, 20.0);
+	hashtable_double_double.print();
+
+	///////////////////////////////////////////
+	// Count function Test
+	///////////////////////////////////////////
+
+	HashTable<int, int> for_count(16);
+	for_count.print();
+
+	for_count.insert(0, 1);
+	for_count.insert(2976934, 2);
+	for_count.insert(38686791, 3);
+	for_count.insert(1547732, 4);
+	for_count.insert(4362, 5);
+	for_count.insert(559794, 6);
+	for_count.insert(6799767, 7);
+	for_count.insert(7687691, 8);
+	for_count.insert(845782, 9);
+	for_count.insert(8063467, 10);
+	for_count.insert(6868, 11);
+	for_count.insert(1669600989, 12);
+	for_count.insert(17815, 13);
+	for_count.insert(151455, 14);
+	for_count.print();
+
+	cout << "Count: " << for_count.count(1669600989) << ' ' << for_count.count(1) << endl <<
+		"Count: " << for_count.Hash(4362) << ' ' << for_count.Hash(2976934) << ' ' << for_count.Hash(0) << ' ' 
+		<< for_count.Hash(6799767) << ' ' << for_count.count(4362) << endl << endl;
+
+	//////////////////////////////////////////////////////////
+	// Task
+	//////////////////////////////////////////////////////////
+
+	/*cout << "Task: " << endl << endl;
+
+	size_t size = 23;
+	for (size_t i = 25; i <= 475; i += 50) {
+		HashTable<int, int> hashtable_fot_task(i, size, -10000, 10000, 0, 1000000000);
+		int count_of_collisions = 0;
+		for (size_t j = 0; j < i; ++j) {
+			if (hashtable_fot_task[j]._flag != 0) {
+				if (hashtable_fot_task.count(hashtable_fot_task[j]._pair._key) > 0) {
+					count_of_collisions++;
+				}
+			}
+		}
+		cout << "Capacity: " << i << " count_of_collisions = " << count_of_collisions << endl;
+	}*/
+
+
+	return 0;
 }
--- hashtable_lab/hashtable_lab.h
+++ hashtable_lab/hashtable_lab.h
@@ -3,9 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include <random>
-#define A 30000
-#define L 14
-#define WORD_LENGHT sizeof(int)*8
+#define WORD_LENGHT sizeof(size_t)*8
 
 using namespace std;
 
@@ -14,6 +12,7 @@ struct Pair {
 	K _key;
 	T _value;
 	Pair(K key, T value) : _key(key), _value(value) {}
+	
 };
 
 template <typename K, typename T>
@@ -22,79 +21,108 @@ struct Node {
 	int _flag;
 	Node() : _pair(Pair<K, T>(0, 0)), _flag(0) {}
 	Node(Pair<K, T> pair, int flag) : _pair(pair), _flag(flag) {}
+	~Node() = default;
 };
 
 template <typename K, typename T>
 class HashTable {
-	int _capacity;
-	int _size;
+
+public:
+	size_t _capacity;
+	size_t _size;
 	Node<K, T>* _ht;
 
-	void clear() {
-		_size = 0;
-		_capacity = 0;
-		delete _ht;
+	void _swap(HashTable<K, T>& other) noexcept {
+		std::swap(_capacity, other._capacity);
+		std::swap(_size, other._size);
+		std::swap(_ht, other._ht);
 	}
 
-	int Hash(K key) {
+	size_t Hash(K key) {
 		if (key < 0) throw runtime_error("Key cann`t be neggative.");
-		int tmp = key * A;
-		int result = ((result >> L) | (result << WORD_LENGHT - L));
+		size_t a = 27510;
+		size_t l = 27;
+		size_t tmp = size_t((key * a)) % size_t(pow(2, 64));
+		size_t result = ((tmp >> l) | (tmp << WORD_LENGHT - l));
 		return result % _capacity;
 	}
 
-	void clear() {
-		_size = 0;
-		_capacity = 0;
-		delete _ht;
-	}
-
-public:
-	int get_size() {
+	size_t get_size() const{
 		return _size;
 	}
 
-	int get_capacity() {
+	size_t get_capacity() const {
 		return _capacity;
 	}
 
-	HashTable(int capasity) : _capacity(capasity), _size(0) {
+	HashTable(size_t capasity) : _capacity(capasity), _size(0) {
 		_ht = new Node<K, T>[capasity];
 	}
 
-	HashTable(const HashTable<K, T> other) {
-		HashTable<K, T> new_ht(other._capacity);
-		for (int i = 0; i < _capacity; ++i) {
-			if (other._ht[i]._flag == 1) {
-				new_ht.insert(other._ht[i]._pair._key, other._ht[i]._pair._value);
+	HashTable(size_t capacity, T min_value, T max_value, K k_min, K k_max) {
+		_capacity = capacity;
+		_size = 0;
+		_ht = new Node<K, T>[_capacity];
+		for (size_t i = 0; i < _capacity; ++i) {
+			K key = (K)(k_min + (double)(rand()) / RAND_MAX * (k_max - k_min + 1));
+			if (key < 0) {
+				key = abs(key);
 			}
+			T value = (T)(min_value + (double)(rand()) / RAND_MAX * (max_value - min_value + 1));
+			insert(key, value);
 		}
-		delete _ht;
-		_ht = new_ht._ht;
-		_capacity = other._capacity;
-		_size = other._size;
 	}
 
-	HashTable operator=(const HashTable other) {
-		HashTable new_ht(other);
-		auto tmp = new_ht._ht;
-		delete new_ht._ht;
-		_ht = new_ht._ht;
+	HashTable(size_t capacity, size_t size, T min_value, T max_value, K k_min, K k_max) {
+		_capacity = capacity;
+		_size = 0;
+		_ht = new Node<K, T>[_capacity];
+		for (size_t i = 0; i < size; ++i) {
+			K key = (K)(k_min + (double)(rand()) / RAND_MAX * (k_max - k_min + 1));
+			if (key < 0) {
+				key = abs(key);
+			}
+			T value = (T)(min_value + (double)(rand()) / RAND_MAX * (max_value - min_value + 1));
+			insert(key, value);
+		}
+	}
+
+	HashTable(const HashTable<K, T>& other) {
 		_capacity = other._capacity;
 		_size = other._size;
+		_ht = new Node<K, T>[_capacity];
+		for (size_t i = 0; i < _capacity; ++i){
+			if (other._ht[i]._flag == 1) {
+				_ht[i]._pair = Pair(other._ht[i]._pair._key, other._ht[i]._pair._value);
+				_ht[i]._flag = 1;
+			}
+		}
+	}
+
+	HashTable<K,T> operator=(const HashTable<K, T>& other) {
+		HashTable<K, T> copy(other);
+		copy._swap(*this);
+		return *this;
+	}
+
+	Node<K, T>& operator[](const size_t index) {
+		return _ht[index];
 	}
 
 	~HashTable() {
-		clear();
+		_capacity = 0;
+		_size = 0;
+		delete[] _ht;
 	}
 
 	void print() const {
 		if (_size == 0) {
-			cout << "Hastable is empty." << endl;
+			cout << "Hastable is empty." << endl << endl;
 			return;
 		}
+		cout << "capacity = " << _capacity << " " << "size = " << _size << endl;
 		cout << "Index and value of Hastable:" << endl;
-		for (int i = 0; i < _capacity; ++i) {
+		for (size_t i = 0; i < _capacity; ++i) {
 			if (_ht[i]._flag == 1) {
 				cout << "[" << i << "] : " << _ht[i]._pair._value << endl;
 			}
@@ -102,9 +130,9 @@ public:
 		cout << endl;
 	}
 
-	bool contains(T value) const {
+	bool contains(T value) {
 		if (_size == 0) return false;
-		for (int i = 0; i < _size; ++i) {
+		for (size_t i = 0; i < _size; ++i) {
 			if (_ht[i]._pair._value == value) {
 				return true;
 			}
@@ -112,27 +140,54 @@ public:
 		return false;
 	}
 
+	/*bool contains(K key) {
+		if (_size == 0) return false;
+		if (key < 0) throw runtime_error("Key cann`t be neggative.");
+		size_t index = Hash(key);
+		if (_ht[index]._flag == 1 && _ht[index]._pair._key == key) {
+			return true;
+		}
+		if (_ht[index]._flag != 0 && _ht[index]._pair._key != key) {
+			size_t start_index = index;
+			size_t  i = 1;
+			do {
+				index = (Hash(key) + i) % _capacity;
+				i++;
+				if (index == start_index) {
+					return false;
+				}
+			} while (_ht[index]._flag != 0 && (_ht[index]._flag == 2 || _ht[index]._pair._key != key));
+
+			if (_ht[index]._flag == 0) return false;
+
+			else {
+				return true;
+			}
+		}
+		else return false;
+	}*/
+
 	T* search(K key) {
 		if (key < 0) throw runtime_error("Key cann`t be neggative.");
-		int index = Hash(key);
-		if (_ht[index]._flag == 1 && ht[index]._pair._key == key) {
-			return new ht[index]._pair._value;
+		size_t index = Hash(key);
+		if (_ht[index]._flag == 1 && _ht[index]._pair._key == key) {
+			return &_ht[index]._pair._value;
 		}
-		if (_ht[index]._flag != 0) {
-			int start_index = index;
-			int  i = 1;
+		if (_ht[index]._flag != 0 && _ht[index]._pair._key != key) {
+			size_t start_index = index;
+			size_t  i = 1;
 			do {
-				index = (Hash(key) + i) % _size;
+				index = (Hash(key) + i) % _capacity;
 				i++;
 				if (index == start_index) {
 					return nullptr;
 				}
 			} while (_ht[index]._flag != 0 && (_ht[index]._flag == 2 || _ht[index]._pair._key != key));
-			if (_ht[index]._flag != 1) return nullptr;
+
+			if (_ht[index]._flag == 0) return nullptr;
 
 			else {
-				T* element = new _ht[index]._pair._value;
-				return element;
+				return &_ht[index]._pair._value;
 			}
 		}
 		else return nullptr;
@@ -141,23 +196,22 @@ public:
 	void insert(K key, T value) {
 		if (key < 0) throw runtime_error("Key cann`t be neggative.");
 		if (_size == _capacity) {
-			_capacity = _capacity * 2;
+			_capacity =_capacity * 2;
 			HashTable<K, T> new_ht(_capacity);
-			for (int i = 0; i < _capacity; ++i) {
+			for (size_t i = 0; i < _capacity; ++i) {
 				if (_ht[i]._flag == 1) {
 					new_ht.insert(_ht[i]._pair._key, _ht[i]._pair._value);
 				}
 			}
-			delete _ht;
-			_ht = new_ht._ht;
+			new_ht._swap(*this);
 		}
-		int index = Hash(key);
+		size_t index = Hash(key);
 		if (_ht[index]._flag == 1) {
-			int  i = 1;
+			size_t  i = 1;
 			do {
-				index = (Hash(key) + i) % _size;
+				index = (Hash(key) + i) % _capacity;
 				i++;
-			} while (_ht[index]._flag != 0);
+			} while (_ht[index]._flag == 1);
 		}
 		_ht[index]._pair = Pair(key, value);
 		_ht[index]._flag = 1;
@@ -166,7 +220,7 @@ public:
 
 	void insert_or_assign(K key, T value) {
 		if (key < 0) throw runtime_error("Key cann`t be neggative.");
-		int index = Hash(key);
+		size_t index = Hash(key);
 		if (_ht[index]._flag != 1) {
 			_ht[index]._pair = Pair(key, value);
 			_ht[index]._flag = 1;
@@ -179,17 +233,17 @@ public:
 
 	bool erase(K key) {
 		if (key < 0) throw runtime_error("Key cann`t be neggative.");
-		int index = Hash(key);
+		size_t index = Hash(key);
 		if (_ht[index]._flag == 0) return false;
-		int start_index = index;
-		int  i = 1;
+		size_t start_index = index;
+		size_t  i = 1;
 		do {
 			if (_ht[index]._flag == 1 && _ht[index]._pair._key == key) {
 				_ht[index]._flag = 2;
 				_size--;
 				return true;
 			}
-			index = (Hash(key) + i) % _size;
+			index = (Hash(key) + i) % _capacity;
 			i++;
 			if (index == start_index) {
 				return false;
@@ -199,20 +253,27 @@ public:
 	}
 
 	int count(K key) {
+		if (key < 0) throw runtime_error("Key cann`t be neggative.");
 		int count = 0;
-		int index = Hash(key);
-		int start_index = index;
-		int  i = 1;
-		do {
-			if (_ht[index]._flag == 1 && _ht[index]._pair._key == key) {
-				return count;
-			}
-			index = (Hash(key) + i) % _size;
-			i++;
-			if (index == start_index) {
-				return false;
-			}
-		} while (_ht[index]._pair != 0 && _ht[index]._pair._key != key);
+		size_t index = Hash(key);
+		if (_ht[index]._flag == 1 && _ht[index]._pair._key == key) {
+			return 0;
+		}
+		if (_ht[index]._flag != 0 && _ht[index]._pair._key != key) {
+			size_t start_index = index;
+			size_t  i = 1;
+			do {
+				if (Hash(_ht[index]._pair._key) == Hash(key)) count++;
+				index = (Hash(key) + i) % _capacity;
+				i++;
+				if (index == start_index) {
+					return 0;
+				}
+			} while (_ht[index]._flag != 0 && (_ht[index]._flag == 2 || _ht[index]._pair._key != key));
+			if (_ht[index]._flag == 0) return 0;
+			else return count;
+		}
 	}
+
 };
 
